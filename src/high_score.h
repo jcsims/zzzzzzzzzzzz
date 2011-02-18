3eed7516c5afebe810b1156b92e7f3279022cfee
Chris Sims <jcsims@gmail.com>
2011-02-18 08:19:01 -0800

    Added high score source, header, and data file.

---
+++ src/high_score.h
@@ -0,0 +1,5 @@
+/* Filename: high_score.h
+ * Authors: Chris Sims	(add your name here as you edit)
+ * Date created: 18 Feb 2011
+ * Description: Definitions and prototypes needed for high score read/write 
+ */
