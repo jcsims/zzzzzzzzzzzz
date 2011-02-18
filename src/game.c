5088f4f2915da27a0032cddfc33db226d58b08fe
Chris Sims <jcsims@gmail.com>
2011-02-18 08:19:48 -0800

    Added game source and header file.

---
+++ src/game.c
@@ -0,0 +1,14 @@
+/* Filename: game.c
+ * Authors: Chris Sims	(add your name here as you edit)
+ * Date created: 18 Feb 2011
+ * Description: Main game engine source file for zzzzzzzzzzzz 
+ */
+
+#include <stdio.h>
+#include "game.h"
+
+int main (int argc, char *argv[]) {
+	puts("Hello, game file reporting.");
+	
+	return 0;
+}
---
+++ src/game.h
@@ -0,0 +1,7 @@
+/* Filename: game.h
+ * Authors: Chris Sims	(add your name here as you edit)
+ * Date created: 18 Feb 2011
+ * Description: Various definitions and prototypes for zzzzzzzzzzzz 
+ */
+
+ #include "maps.h"
