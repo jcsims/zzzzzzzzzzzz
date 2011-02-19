6dc6aa7b88928e131c78445a8adc50ffa532a7ec
Chris Sims <jcsims@gmail.com>
2011-02-18 16:14:11 -0800

    Added initial init logic to map.c, and data structure to map.h

--- src/game.c
+++ src/game.c
@@ -4,10 +4,21 @@
  * Description: Main game engine source file for zzzzzzzzzzzz 
  */
 
-#include <stdio.h>
+
 #include "game.h"
 
 int main (int argc, char *argv[]) {
+
+	d_game_state newgame;
+
+	initscr(); 				//Start ncurses
+	keypad(stdscr, true); 	//enable use of the arrow keys
+
+	if (!init_game(&newgame)) {
+		puts("Error: not able to init new game.")
+		exit(1);
+	}
+
 	puts("Hello, game file reporting.");
 	
 	return 0;
--- src/game.h
+++ src/game.h
@@ -4,4 +4,17 @@
  * Description: Various definitions and prototypes for zzzzzzzzzzzz 
  */
 
- #include "maps.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <ncurses.h>
+#include "maps.h"
+
+typedef struct d_game_state {
+	bool paused;
+	bool game_complete;
+}
+
+int init_game (d_game_state *newgame) {
+ 	newgame->paused = false;
+ 	newgame->game_complete = false;
+}
