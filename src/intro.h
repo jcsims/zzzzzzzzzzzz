173ae85aa2773f96bee148c9b9e28ba1e6040338
Chris Sims <jcsims@gmail.com>
2011-02-20 18:09:48 -0800

    Added skeleton intro source and head file

---
+++ src/intro.h
@@ -0,0 +1,7 @@
+/* Filename: intro.h
+ * Authors: Chris Sims	(add your name here as you edit)
+ * Date created: 20 Feb 2011
+ * Description: Constants, function declarations for intro screen/pause menu
+ */
+
+ 
