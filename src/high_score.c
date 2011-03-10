1e72cf3895bea5a4d04de6eeee8cc8cfdc06e4f4
Chris Sims <jcsims@gmail.com>
2011-03-10 07:44:04 -0800

    Cleaned out git hub repository before pushing all new changes.

--- src/high_score.c
+++
@@ -1,7 +0,0 @@
-/* Filename: high_score.c
- * Authors: Chris Sims	(add your name here as you edit)
- * Date created: 18 Feb 2011
- * Description: Read in and save the game high score info 
- */
-
- //TODO: Make sure to block interrupts while writing and reading the file
--- src/high_score.h
+++
@@ -1,5 +0,0 @@
-/* Filename: high_score.h
- * Authors: Chris Sims	(add your name here as you edit)
- * Date created: 18 Feb 2011
- * Description: Definitions and prototypes needed for high score read/write 
- */
